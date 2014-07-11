80719c20061b09f31945ac6995e6bfbd5eaa73a5
valmaev <almaev.vladimir@gmail.com>
2014-07-11 16:45:14 +0400

    Added YoutrackSprintProvider

---
+++ src/main/java/org/valmaev/teamcity/server/YoutrackSprintProvider.java
@@ -0,0 +1,6 @@
+package org.valmaev.teamcity.server;
+
+import jetbrains.buildServer.serverSide.parameters.AbstractBuildParametersProvider;
+
+public class YoutrackSprintProvider extends AbstractBuildParametersProvider {
+}

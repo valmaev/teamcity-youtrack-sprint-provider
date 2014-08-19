e097176e16688de71692783ee0dc3faa0369c27f
valmaev <almaev.vladimir@gmail.com>
2014-08-20 00:07:22 +0400

    Changed youTrackUri parameter to connection parameter of type IssueTrackerConnection.

--- src/main/java/org/valmaev/teamcity/server/dataaccess/YoutrackSprintNameProvider.java
+++ src/main/java/org/valmaev/teamcity/server/dataaccess/YoutrackSprintNameProvider.java
@@ -1,43 +1,48 @@
 package org.valmaev.teamcity.server.dataaccess;
 
+import org.valmaev.teamcity.server.domain.IssueTrackerConnection;
 import org.valmaev.teamcity.server.domain.Sprint;
 import org.valmaev.teamcity.server.domain.SprintNameProvider;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
-import java.net.URI;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import static org.glassfish.jersey.client.authentication.HttpAuthenticationFeature.HTTP_AUTHENTICATION_BASIC_PASSWORD;
+import static org.glassfish.jersey.client.authentication.HttpAuthenticationFeature.HTTP_AUTHENTICATION_BASIC_USERNAME;
+
 public class YoutrackSprintNameProvider implements SprintNameProvider {
 
     private final Client _restClient;
-    private final URI _youtrackUri;
+    private final IssueTrackerConnection _connection;
     private final Comparator<Sprint> _sprintComparator;
 
     public YoutrackSprintNameProvider(
             Client restClient,
-            URI youtrackUri,
+            IssueTrackerConnection connection,
             Comparator<Sprint> sprintComparator) {
         if (restClient == null)
             throw new IllegalArgumentException("Parameter 'restClient' can't be null");
-        if (youtrackUri == null)
-            throw new IllegalArgumentException("Parameter 'youtrackUri' can't be null");
+        if (connection == null)
+            throw new IllegalArgumentException("Parameter 'connection' can't be null");
         if (sprintComparator == null)
             throw new IllegalArgumentException("Parameter 'sprintComparator' can't be null");
 
         _restClient = restClient;
-        _youtrackUri = youtrackUri;
+        _connection = connection;
         _sprintComparator = sprintComparator;
     }
 
     @Override
     public String getCurrentSprintName(String projectId) {
-        List<Sprint> sprints = _restClient.target(_youtrackUri)
+        List<Sprint> sprints = _restClient.target(_connection.getAddress())
                 .path("/rest/admin/project/" + projectId + "/version")
                 .request(MediaType.APPLICATION_JSON)
+                .property(HTTP_AUTHENTICATION_BASIC_USERNAME, _connection.getLogin())
+                .property(HTTP_AUTHENTICATION_BASIC_PASSWORD, _connection.getPassword())
                 .get(new GenericType<List<Sprint>>() {});
         Collections.sort(sprints, _sprintComparator);
 
--- src/test/java/org/valmaev/teamcity/server/dataaccess/YoutrackSprintNameProviderTestCase.java
+++ src/test/java/org/valmaev/teamcity/server/dataaccess/YoutrackSprintNameProviderTestCase.java
@@ -2,6 +2,7 @@ package org.valmaev.teamcity.server.dataaccess;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.valmaev.teamcity.server.domain.IssueTrackerConnection;
 import org.valmaev.teamcity.server.domain.Sprint;
 
 import javax.ws.rs.client.Client;
@@ -16,29 +17,27 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+import static org.glassfish.jersey.client.authentication.HttpAuthenticationFeature.HTTP_AUTHENTICATION_BASIC_PASSWORD;
+import static org.glassfish.jersey.client.authentication.HttpAuthenticationFeature.HTTP_AUTHENTICATION_BASIC_USERNAME;
 import static org.mockito.Mockito.*;
 import static org.testng.Assert.assertEquals;
 import static org.valmaev.teamcity.server.domain.SprintFactory.createSprint;
+
 
 public class YoutrackSprintNameProviderTestCase {
 
     @SuppressWarnings("unchecked")
     private static YoutrackSprintNameProvider createSystemUnderTest(
             Client restClient,
-            URI youtrackUri,
+            IssueTrackerConnection connection,
             Comparator<Sprint> sprintComparator)
             throws URISyntaxException {
         return new YoutrackSprintNameProvider(
                 restClient == null ? mock(Client.class) : restClient,
-                youtrackUri == null ? createUriDummy() : youtrackUri,
+                connection == null ? mock(IssueTrackerConnection.class) : connection,
                 sprintComparator == null
                         ? (Comparator<Sprint>) mock(Comparator.class)
                         : sprintComparator);
-    }
-
-    private static URI createUriDummy()
-            throws URISyntaxException {
-        return new URI("http://example.com");
     }
 
     private static Client createClientStub(List<Sprint> responseContent) {
@@ -46,27 +45,37 @@ public class YoutrackSprintNameProviderTestCase {
         WebTarget targetStub = mock(WebTarget.class);
         Builder builderStub = mock(Builder.class);
 
-        when(result.target(any(URI.class))).thenReturn(targetStub);
-        when(targetStub.path(any(String.class))).thenReturn(targetStub);
-        when(targetStub.request(any(String.class))).thenReturn(builderStub);
-        when(builderStub.get(new GenericType<List<Sprint>>() {})).thenReturn(responseContent);
+        when(result.target(any(URI.class)))
+                .thenReturn(targetStub);
+        when(targetStub.path(any(String.class)))
+                .thenReturn(targetStub);
+        when(targetStub.request(any(String.class)))
+                .thenReturn(builderStub);
+        when(builderStub.property(any(String.class), any(Object.class)))
+                .thenReturn(builderStub);
+        when(builderStub.get(new GenericType<List<Sprint>>() {}))
+                .thenReturn(responseContent);
 
         return result;
     }
 
     private static Builder createBuilderTestDouble(
             Client clientTestDouble,
-            URI youtrackUri,
+            IssueTrackerConnection connection,
             String projectId,
             List<Sprint> responseContent) {
         WebTarget targetStub = mock(WebTarget.class);
         Builder result = mock(Builder.class);
 
-        when(clientTestDouble.target(youtrackUri))
+        when(clientTestDouble.target(connection.getAddress()))
                 .thenReturn(targetStub);
         when(targetStub.path("/rest/admin/project/" + projectId + "/version"))
                 .thenReturn(targetStub);
         when(targetStub.request(MediaType.APPLICATION_JSON))
+                .thenReturn(result);
+        when(result.property(HTTP_AUTHENTICATION_BASIC_USERNAME, connection.getLogin()))
+                .thenReturn(result);
+        when(result.property(HTTP_AUTHENTICATION_BASIC_PASSWORD, connection.getPassword()))
                 .thenReturn(result);
         when(result.get(new GenericType<List<Sprint>>() {}))
                 .thenReturn(responseContent);
@@ -77,13 +86,20 @@ public class YoutrackSprintNameProviderTestCase {
     public void getCurrentSprintName_always_shouldSendProperGetRequestThroughRestClient()
             throws Exception {
         // Fixture setup
-        final URI youtrackUriDummy = createUriDummy();
+        final IssueTrackerConnection connectionStub =
+                mock(IssueTrackerConnection.class, RETURNS_DEEP_STUBS);
         final String inputProjectId = "foo";
 
         Client clientStub = mock(Client.class);
-        Builder builderMock =
-                createBuilderTestDouble(clientStub, youtrackUriDummy, inputProjectId, new ArrayList<Sprint>());
-        YoutrackSprintNameProvider sut = createSystemUnderTest(clientStub, youtrackUriDummy, null);
+        Builder builderMock = createBuilderTestDouble(
+                clientStub,
+                connectionStub,
+                inputProjectId,
+                new ArrayList<Sprint>());
+        YoutrackSprintNameProvider sut = createSystemUnderTest(
+                clientStub,
+                connectionStub,
+                null);
 
         // Exercise
         sut.getCurrentSprintName(inputProjectId);
@@ -99,7 +115,10 @@ public class YoutrackSprintNameProviderTestCase {
             String expected)
             throws Exception {
         Client clientStub = createClientStub(responseContent);
-        YoutrackSprintNameProvider sut = createSystemUnderTest(clientStub, null, sprintComparator);
+        YoutrackSprintNameProvider sut = createSystemUnderTest(
+                clientStub,
+                null,
+                sprintComparator);
 
         String actual = sut.getCurrentSprintName("foo");
 

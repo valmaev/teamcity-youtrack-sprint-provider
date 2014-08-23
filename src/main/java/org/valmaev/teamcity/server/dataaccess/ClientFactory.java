f5a31c74dcfce68e3eb3c0df85a58ecc5ed94e9b
valmaev <almaev.vladimir@gmail.com>
2014-08-23 23:52:12 +0400

    Removed basic HttpAuthenticationFeature

--- src/main/java/org/valmaev/teamcity/server/dataaccess/ClientFactory.java
+++ src/main/java/org/valmaev/teamcity/server/dataaccess/ClientFactory.java
@@ -1,16 +1,14 @@
 package org.valmaev.teamcity.server.dataaccess;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 
 public class ClientFactory {
 
-    public Client create(String login, String password) {
+    public Client create() {
         return ClientBuilder.newClient()
-                .register(new JacksonJsonProvider(ObjectMapperFactory.create()))
-                .register(HttpAuthenticationFeature.basic(login, password));
+                .register(new JacksonJsonProvider(ObjectMapperFactory.create()));
     }
 }

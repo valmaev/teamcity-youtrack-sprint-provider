1fb9cc1330d68cf07ea92f030baee68d347ee531
valmaev <almaev.vladimir@gmail.com>
2014-08-28 14:42:03 +0400

    Added registration of HttpAuthenticationFeature.

--- src/main/java/org/valmaev/teamcity/server/dataaccess/ClientFactory.java
+++ src/main/java/org/valmaev/teamcity/server/dataaccess/ClientFactory.java
@@ -1,6 +1,7 @@
 package org.valmaev.teamcity.server.dataaccess;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -9,6 +10,7 @@ public class ClientFactory {
 
     public Client create() {
         return ClientBuilder.newClient()
-                .register(new JacksonJsonProvider(ObjectMapperFactory.create()));
+                .register(new JacksonJsonProvider(ObjectMapperFactory.create()))
+                .register(HttpAuthenticationFeature.basicBuilder().build());
     }
 }
